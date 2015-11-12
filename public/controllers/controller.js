8871bede7c6c292339e381a9defb3a24f0733350
Sujan <sujanrao17@gmail.com>
2015-11-12 12:25:42 -0600

    Added functionality to buttons

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,11 +2,11 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http){
 	console.log("Hello from controller");
-
+$scope.bool = false;
 	var refresh = function(){
 		$http.get('/controller').success(function(response){
 
-		console.log("Data recieves");
+	$scope.bool = false;
 		$scope.contactlist = response;
 		$scope.contact="";
 
@@ -18,7 +18,7 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http){
 	$scope.addContact = function(){
 		console.log($scope.contact);
 		$http.post('/controller', $scope.contact).success(function(response){
-			console.log(response);
+			$scope.bool = false;
 			refresh();
 		});
 	};
@@ -26,10 +26,31 @@ myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http){
 	$scope.remove =function(id){
 		console.log(id);
 		$http.delete('/controller/' + id).success(function(response){
-			console.log(response);
+$scope.bool = false;
+			console.log("From delete"+response);
 			refresh();
 		});
 
+	};
+
+	$scope.edit =function(id){
+		console.log("Id from edit"+id);
+		$http.get('/controller/' + id).success(function(response){
+			$scope.bool = true;
+			$scope.contact = response;
+			
+			
+		});
+	};
+
+	$scope.update =function(){
+		
+		$http.put('/controller/' + $scope.contact._id, $scope.contact ).success(function(response){
+			console.log(response);
+			
+			refresh();
+			
+		});
 	};
 	
 
--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyparser.json());
 
 app.get('/controller', function(req,res){
-	console.log("recieved a get request");
+	
 	db.contactlist.find(function(err, docs){
-		console.log(docs);
+		
 		res.json(docs);
 	});
 	
@@ -28,7 +28,29 @@ app.post('/controller', function(req,res){
 
 app.delete('/controller/:id', function(req,res){
 	var id = req.params.id;
+	console.log("Delete Called");
 	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, docs){
+		
+		res.json(docs);
+	});
+
+});
+
+app.get('/controller/:id', function(req,res){
+	var id = req.params.id;
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, docs){
+		console.log("inside server edit"+ docs);
+		res.json(docs);
+	});
+
+});
+
+app.put('/controller/:id', function(req,res){
+	var id = req.params.id;
+	db.contactlist.findAndModify({Query: {_id: mongojs.ObjectId(id)},
+		update:{$set:{name: req.body.name, email: req.body.email, number:req.body.number}},
+		new: false}, function(err, docs){
+		
 		res.json(docs);
 	});
 
