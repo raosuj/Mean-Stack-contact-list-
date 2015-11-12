21742962292e57024d466bc9d60cca814588882a
Sujan <sujanrao17@gmail.com>
2015-11-11 21:49:08 -0600

    Added Background and buttons

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
- 
+var bodyparser = require('body-parser');
+
 
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyparser.json());
+
 app.get('/controller', function(req,res){
 	console.log("recieved a get request");
 	db.contactlist.find(function(err, docs){
@@ -13,6 +16,22 @@ app.get('/controller', function(req,res){
 		res.json(docs);
 	});
 	
+});
+
+app.post('/controller', function(req,res){
+	console.log(req.body);
+	db.contactlist.insert(req.body, function(err, docs){
+		res.json(docs);
+	});
+
+});
+
+app.delete('/controller/:id', function(req,res){
+	var id = req.params.id;
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, docs){
+		res.json(docs);
+	});
+
 });
 app.listen(3000);
 console.log('server running');
