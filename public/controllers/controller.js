daf0e0fa6a30be551dba50a0a7506c9fa02232ab
Sujan <sujanrao17@gmail.com>
2015-11-11 21:47:35 -0600

    Added Background image and Buttons

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,18 +3,34 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http){
 	console.log("Hello from controller");
 
-
-	$http.get('/controller').success(function(response){
+	var refresh = function(){
+		$http.get('/controller').success(function(response){
 
 		console.log("Data recieves");
 		$scope.contactlist = response;
+		$scope.contact="";
 
 	});
 
+	};
+	refresh();
+
 	$scope.addContact = function(){
-		console.log($scope.contact)
+		console.log($scope.contact);
+		$http.post('/controller', $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
 	};
 	
+	$scope.remove =function(id){
+		console.log(id);
+		$http.delete('/controller/' + id).success(function(response){
+			console.log(response);
+			refresh();
+		});
+
+	};
 	
 
 }]);
